a3acbdd90d7b931fa9e831ae0e36f1e8894ac9b6
nmarks3 <NMarks3@my.wctc.edu>
2018-10-07 14:47:30 -0500

    re put in the port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
-const port = process.env.port;
+const port = process.env.port || 3000;
 var app = express();
 
 // setup partials !! MIDDLEWARE !!
